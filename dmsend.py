639d37ae236f685da64d72bca1f44580b52c4b9b
dldlsgml0626 <72138456+dldlsgml0626@users.noreply.github.com>
2020-09-30 21:21:21 +0900

    Add files via upload

---
+++ dmsend.py
@@ -0,0 +1,39 @@
+
+# 봉순#6959 : MASS DM BOT SOURCE
+
+
+import discord
+import asyncio
+import datetime
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("DM봇이 정상적으로 실행되었습니다.")
+    game = discord.Game('DM Bot')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+#/dm {할말}로 전체DM 전송
+@client.event
+async def on_message(message):
+    if message.content.startswith('/dm'):
+        for i in message.guild.members:
+            if i.bot == True:
+                pass
+            else:
+                try:
+                    msg = message.content[4:]
+                    #메시지 관리권한이 있을시 사용가능
+                    if message.author.guild_permissions.manage_messages:
+                        embed = discord.Embed(color=0x1DDB16, timestamp=message.created_at)
+                        embed.add_field(name="공지", value=msg, inline=True)
+                        embed.set_footer(text="˃̶᷄‧̫ ˂̶᷅๑#7777")
+                        await i.send(embed=embed)
+                except:
+                    pass
+
+
+client.run('NzYwMzA5NDA3MTQ1MTMyMDYz.X3KLUA.KzKtMDOtKYxyrGtVzOo5e0v83Ug')
+
+
